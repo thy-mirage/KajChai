9c1fc35a14de2ee035a45c57ac31955638233414
thy-mirage <2005084@ugrad.cse.buet.ac.bd>
2025-09-18 05:47:06 +0600

    Force rebuild: Update version and add env debug

--- KajChaiFrontend/src/main.jsx
+++ KajChaiFrontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './i18n' // Import i18n configuration
+import './debug-env.js' // Debug environment variables
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
